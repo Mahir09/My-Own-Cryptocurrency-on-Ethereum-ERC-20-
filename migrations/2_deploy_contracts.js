457e5c7e85f788ebe3f8f66f88f84cf5194aebcb
Mahir09 <mahirshaikh932002@gmail.com>
2022-07-15 11:56:30 +0530

    Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const DappToken = artifacts.require("DappToken");
+var DappTokenSale = artifacts.require("DappTokenSale");
 
-module.exports = function (deployer) {
-  deployer.deploy(DappToken, 1000000);
+module.exports = function(deployer) {
+  deployer.deploy(DappToken, 1000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000; // in wei
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+  });
 };
